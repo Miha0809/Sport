591d8f4cf619c6d95083529591636681ef8aaf31
Miha0809 <miha080104@gmail.com>
2024-04-06 18:57:46 +0200

    Update .gitignore

---
+++ Sport.API/Sport.API/Models/DTOs/User/UserShowDto.cs
@@ -0,0 +1,14 @@
+namespace Sport.API.Models.DTOs;
+
+public class UserShowDto
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    
+    public required string Email { get; set; }
+    public required bool EmailConfirmed { get; set; }
+    
+    public string? PhoneNumber { get; set; }
+    public required bool PhoneNumberConfirmed { get; set; }
+    public required bool TwoFactorEnabled { get; set; }
+}
---
+++ Sport.API/Sport.API/Models/DTOs/User/UserUpdateDto.cs
@@ -0,0 +1,7 @@
+namespace Sport.API.Models.DTOs;
+
+public class UserUpdateDto
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+}
