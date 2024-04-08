f88764b82bcee1f8512f7f5bd4b39073fb4a4a8e
Miha0809 <miha080104@gmail.com>
2024-04-08 21:04:03 +0200

    UserByEmail

--- Sport.API/Sport.API/Controllers/UserController.cs
+++ Sport.API/Sport.API/Controllers/UserController.cs
@@ -1,6 +1,33 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sport.API.Models.DTOs.User;
+using Sport.API.Services;
+
 namespace Sport.API.Controllers;
 
-public class UserController
+/// <summary>
+/// Котнроллер для роботи з користувачами для пошуку.
+/// </summary>
+/// <param name="context"></param>
+/// <param name="mapper"></param>
+public class UserController(SportDbContext context, IMapper mapper) : Controller
 {
-    
+    /// <summary>
+    /// Користувач по електронній пошті.
+    /// </summary>
+    /// <param name="email">Електронна пошта.</param>
+    /// <returns></returns>
+    [HttpGet("{email}")]
+    public async Task<IActionResult> UserByEmail(string email)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(user => user.Email!.Equals(email));
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(mapper.Map<UserPublicShowDto>(user));
+    }
 }
