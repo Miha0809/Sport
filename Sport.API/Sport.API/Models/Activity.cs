394723142e511d8c641ebc21cd7e1ffd0c3e5ab4
Miha0809 <miha080104@gmail.com>
2024-04-15 12:47:11 +0200

    Add activity model

---
+++ Sport.API/Sport.API/Models/Activity.cs
@@ -0,0 +1,41 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Sport.API.Models;
+
+/// <summary>
+/// Активність.
+/// </summary>
+public class Activity
+{
+    /// <summary>
+    /// Ідентифікатор.
+    /// </summary>
+    [Key]
+    public int Id { get; set; }
+ 
+    /// <summary>
+    /// Початок активності.
+    /// </summary>
+    public DateTime Start { get; set; }
+    
+    /// <summary>
+    /// Кінець активності.
+    /// </summary>
+    public DateTime End { get; set; }
+    
+    /// <summary>
+    /// Дистанція у метрах.
+    /// </summary>
+    public double Distance { get; set; }
+    
+    /// <summary>
+    /// Поточка швидкість.
+    /// </summary>
+    public double Speed { get; set; }
+    
+    /// <summary>
+    /// Автор активності.
+    /// </summary>
+    public virtual User? User { get; set; }
+}
